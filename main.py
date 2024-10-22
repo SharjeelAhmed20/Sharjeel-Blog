a90972a1c14590b44e3432763d7e7e4182141a24
Sharjeel Ahmed <sharjeel.100daysofcode@gmail.com>
2024-10-22 21:01:21 +0500

    initial commit create Procfile

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ from forms import CreatePostForm,RegisterForm,LoginForm,CommentForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
